2b1a5399687c81e753244ff3f5529f1f96a20def
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2021-02-14 21:03:04 -0500

    Added transition to list in sidebar to show border on hover

--- src/comps/ui/Sidebar.svelte
+++ src/comps/ui/Sidebar.svelte
@@ -15,17 +15,27 @@
     main {
         text-align: right;
         padding: 0 8px;
-        background: rgb(252, 250, 250);
+        background: rgb(240, 237, 237);
         letter-spacing: 1px;
         font-size: 95%;
     }
     h2 {
-        padding-bottom: 20px;
+        padding: 8px 0;
         color: rgb(101, 99, 99);
+    }
+    ul {
+        padding: 8px 0;
     }
     li {
         list-style: none;
-        padding: 8px 0;
+        padding: 8px;
+        margin: 4px auto;
+        background: white;
+        border: 1px solid transparent;
+        transition: 250ms all ease-in-out;
+    }
+    li:hover {
+        border-color: rgb(198, 192, 192);
     }
     @media (max-width: 700px) {
         main {
